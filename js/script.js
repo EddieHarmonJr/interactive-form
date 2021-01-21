bbeb40877a7b0ac761967cec7034a4a9c67d73b0
EddieHarmonJr <eddieharmon@ac.c4q.nyc>
2021-01-21 00:07:56 -0500

    create variables and function to remove other job role field when dropdown is clicked

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('Test');
+const jobTitle = document.getElementById('title');
+const otherRole = document.getElementById("other-job-role");
+
+
+jobTitle.addEventListener('click', hideJobRole);
+
+
+function hideJobRole() {
+  otherRole.style.display = 'none';
+}
